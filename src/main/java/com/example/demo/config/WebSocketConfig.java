e5f06c1cd1eb825cd8a4d2e4c2b4212b0e8a89f9
Brannigan CHATEAU <brannigan.pro@gmail.com>
2023-07-12 20:59:31 +0200

    Testing prod

--- src/main/java/com/example/demo/config/WebSocketConfig.java
+++ src/main/java/com/example/demo/config/WebSocketConfig.java
@@ -13,7 +13,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry){
-        registry.addEndpoint("/socket").setAllowedOrigins("http://localhost:4200/").withSockJS();
+        registry.addEndpoint("/socket").setAllowedOrigins("https://stream-ur-files.vercel.app/").withSockJS();
     }
 
     @Override
