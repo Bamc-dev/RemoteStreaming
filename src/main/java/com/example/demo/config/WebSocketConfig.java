64c9dd4eb282a300fe9d811e9f951d67c5eb4387
Brannigan CHATEAU <brannigan.pro@gmail.com>
2023-09-13 01:12:27 +0200

    I want to Sleep Please !

--- src/main/java/com/example/demo/config/WebSocketConfig.java
+++ src/main/java/com/example/demo/config/WebSocketConfig.java
@@ -1,25 +1,15 @@
 package com.example.demo.config;
 
+import com.example.demo.component.SocketHandler;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.*;
 
 @Configuration
-@EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-
-
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry){
-        registry.addEndpoint("/socket").setAllowedOrigins("*");
-    }
-
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry registry){
-        registry.enableSimpleBroker("/media");
-        registry.setApplicationDestinationPrefixes("/api");
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(new SocketHandler(), "/socket").setAllowedOrigins("*");
 
     }
 }
